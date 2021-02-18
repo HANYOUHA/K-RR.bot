c52a9044b5bbd624dee0fc608ad527b710be049d
triplejblog <triplej03@naver.com>
2021-02-18 23:43:36 +0900

    test

--- K-RR.bot(price).py
+++ K-RR.bot(price).py
@@ -6,15 +6,12 @@ import pandas as pd
 
 f_path = "~/Documents/PriceLogger/price_list.csv"
 last_row = pd.read_csv(f_path).iloc[-1]
-for c in last_row:
-    print(str(c))
 
-'''
 message = "현재 "+ last_row[0] +" RR 인게임 자원 가격은 다음과 같습니다.\n석유: "
-                   + oil +"\n광물: " + ore + "\n우라늄: " + uranium + "\n다이아몬드: "
-                   + diamonds + "\n헬륨: " + helium + "\n라이발륨: " + rivalium +
-                   "\n탱크: " + tanks + "\n전투기: " + aircrafts + "\n미사일:" + missiles +
-                   "\n폭격기: " + bombers + "\n드론: " + drones
+                   + last_row[1]  +"\n광물: " + last_row[2]
+
+print(message)
+'''
 client = commands.Bot(command_prefix='!')
 
 @client.command()
