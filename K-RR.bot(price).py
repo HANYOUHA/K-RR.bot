b8265f1658ab2e193056c04b73d91570d85aa4c3
triplejblog <triplej03@naver.com>
2021-02-18 20:20:32 +0900

    get last row of csv

--- K-RR.bot(price).py
+++ K-RR.bot(price).py
@@ -1,6 +1,19 @@
 import discord
 from discord.ext import commands
 import time
+import csv
+import pandas as pd
+
+
+f_path = "~/Documents/PriceLogger/price_list.csv"
+last_row = pd.read_csv(f_path).iloc[-1]
+print(last_row)
+
+'''
+f = open(f_path, 'r', encoding='utf-8')
+rdr = csv.reader(f)
+for line in rdr:
+    prin
 
 client = commands.Bot(command_prefix='!')
 
@@ -17,3 +30,4 @@ async def 가격(ctx):
                    "\n폭격기: " + bombers + "\n드론: " + drones)
 
 client.run(디코봇 토큰)
+'''
